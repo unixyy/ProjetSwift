0b03aa5eb9281e7aa49590e46f30674aae6d14ff
LucasDarie <73884400+LucasDarie@users.noreply.github.com>
2021-12-05 22:16:02 +0100

    ajout du mode de jeu difficile
    ajout d'un iterateur sur les carrés

--- Lucas&Assiah/JeuProtocol.swift
+++ Lucas&Assiah/JeuProtocol.swift
@@ -25,11 +25,13 @@
 /// nbPieceLigne : Jeu x Int -> Int // renvoie le nombre de piece sur une ligne
 /// nbPieceColonne : Jeu x Int -> Int // renvoie le nombre de piece sur une colonne
 /// nbPieceDiagonale: Jeu x Int -> Int // renvoie le nombre de piece sur une diagonale
+/// nbPieceCarre: Jeu x (Int, Int) -> Int // renvoie le nombre de piece sur un carré
 /// makeItReserve : Jeu -> PiecesIterator // iterator sur toutes les pieces de la reserve
 /// makeIterator : Jeu -> PositionIterator // iterator sur toutes les positions du jeu
 /// MakeItLigne : Jeu x Int-> LigneIterator // iterator sur toutes les pièces d'une ligne
 /// MakeItColonne : Jeu x Int-> ColonneIterator // iterator sur toutes les pièces d'une colonne
 /// MakeItDiagonale : Jeu x Bool -> DiagonaleIterator // iterator sur toutes les pièces d'une diagonale
+/// makeItCarre : Jeu x (Int, Int) -> CarreIterator // iterator sur toutes les pièces d'un carré
 /// compare : Jeu x Piece x Piece x Piece x Piece -> Bool
 /// 
 /// 
@@ -49,8 +51,11 @@
 
 
 protocol JeuProtocol : Sequence {
+    /// init : modeDifficile : Bool -> Jeu
     /// créer un nouveau jeu
-    init()
+    /// modeDifficile : Bool si le mode de jeu est difficile, 
+    /// alors il y a la possibilié de gagner en posant 4 pièces avec une caracteristique identique en formant un carré de 2x2
+    init(modeDifficile: Bool)
 
     /// nbPieceReserve : Jeu -> Int
     /// indique le nombre de pieces restantes dans la reserve
@@ -101,6 +106,27 @@ protocol JeuProtocol : Sequence {
     // diagonale decroissante (true) : de (1, 1) à (4, 4) \ 
     // diagonale croissante (false) : de (1, 4) à (4, 1) /
     func nbPieceDiagonale(decroissante: Bool) -> Int
+   
+    // renvoie le nombre de piece dans un carre
+    // un carré de position est un groupe de 4 positions formant un carré de 2x2 
+    // avec comme identifiant la coordonéee de la position en haut à gauche
+    // il y a 9 carrés : (1, 1), (3, 1), (1, 3), (3, 3)
+    //                 : (2, 1), (1, 2), (3, 2), (2, 3)
+    //                 : (2, 2)
+    /*
+        |------|------|------|------|       
+        |  01  |  02  |  03  |  04  |
+        |------|------|------|------|
+     01 |(1, 1)|(2, 1)|(3, 1)| BRVG |
+        |------|------|------|------|
+     02 |(1, 2)|(2, 2)|(3, 2)| BRVG |
+        |------|------|------|------|
+     03 |(1, 3)|(2, 3)|(3, 3)| BRVG |
+        |------|------|------|------|
+     04 | BRVG | BRVG | BRVG | BRVG |
+        |------|------|------|------|
+    */
+    func nbPieceCarre(posCarre: (Int, Int)) -> Int
 
     // makeIterator : Jeu -> Piece
     // iterator sur toutes les pieces de la reserve
@@ -126,6 +152,13 @@ protocol JeuProtocol : Sequence {
     //     : si une position de la diagonale ne contient pas de piece, on passe à la position suivante avant de return
     func makeItDiagonale(decroissante:Bool) -> DiagonaleIterator
 
+   // iterator sur toutes les pièces d'un carré
+    //     : si une position du carré ne contient pas de piece, on passe à la position suivante avant de return
+    // voir nbPieceCarre pour les positions d'un carré
+    // exemple, si on donne la position (1,1), l'iterator va renvoyer les Pieces situées dans les Positions aux coordonnées (1,1) -> (2, 1) -> (1, 2) -> (2,2) puis nil à la fin de l'iteration
+    // si chacune des positions contiens bien une piece. 
+    func makeItCarre(posCarre:(Int, Int)) -> CarreIterator
+   
     /// compare : Jeu x Piece x Piece x Piece x Piece -> Bool
     /// compare 4 pieces et renvoie si elles ont au moins une caractéristique en commun
     func compare(p1: Piece, p2 : Piece, p3 : Piece, p4 : Piece) -> Bool
@@ -152,6 +185,11 @@ protocol ColonneIteratorProtocol : IteratorProtocol {
 }
 
 protocol DiagonaleIteratorProtocol : IteratorProtocol {
+    init()
+    next() -> Piece?
+}
+
+protocol CarreIteratorProtocol : IteratorProtocol {
     init()
     next() -> Piece?
 }
